aba30083c6ff1baec5abd91d23e1213b687361fc
lyylyylyylyy <lyyzju@outlook.com>
2020-05-01 22:43:10 +0800

    origin map

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Map from './components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <Map></Map>
+    </div>
+  );
+}
+
+export default App;
