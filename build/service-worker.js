fc9694547ebb0e8c8e3e9627489d4d8adced791a
lyylyylyylyy <lyyzju@outlook.com>
2020-05-04 23:41:18 +0800

    map with data

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-map/precache-manifest.1e06fc8e9b256a196de1cf8edb636d20.js"
+  "/covid19-map/precache-manifest.b5a59731f96da4dbc2964ed60ef9986d.js"
 );
 
 self.addEventListener('message', (event) => {
