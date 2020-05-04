a57e1c5ebe8b0f0fc0fe81026031aed9d868df38
lyylyylyylyy <lyyzju@outlook.com>
2020-05-04 23:34:28 +0800

    map with data

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.484aef5c45290dd66d6ac54bfb972d47.js"
+  "/covid19-map/precache-manifest.1e06fc8e9b256a196de1cf8edb636d20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19-map/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React ,  { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
 
-function App() {
-  return (
-    <div className="App">
-      <Map></Map>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <span id = "title">新冠肺炎世界疫情地图</span>
+        <Map className = "map"></Map>
+      </div>
+    );
+  }
 }
 
 export default App;
