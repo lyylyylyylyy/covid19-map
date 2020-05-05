97af25cadaadd882d46ea91ad78cc6db0128a7f2
lyylyylyylyy <lyyzju@outlook.com>
2020-05-05 12:02:17 +0800

    Updates

--- precache-manifest.68b9c75cc6243c136e92b55b350fd7fc.js
+++ precache-manifest.68b9c75cc6243c136e92b55b350fd7fc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8df20ca06bb9dda22b24c06cd62851a7",
+    "revision": "8744ed9b5aa3e66601b0524f5e831bad",
     "url": "/covid19-map/index.html"
   },
   {
-    "revision": "2e0a2d36c8c5e6ab0f06",
+    "revision": "5b3899a1b57ffa4e337e",
     "url": "/covid19-map/static/css/main.89b5e164.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19-map/static/js/2.d3d029fd.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e0a2d36c8c5e6ab0f06",
-    "url": "/covid19-map/static/js/main.0bbeff24.chunk.js"
+    "revision": "5b3899a1b57ffa4e337e",
+    "url": "/covid19-map/static/js/main.7a1789a6.chunk.js"
   },
   {
     "revision": "61bdcdc074e14bb48763",
