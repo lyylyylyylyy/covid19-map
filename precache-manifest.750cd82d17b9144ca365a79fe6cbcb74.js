3ee82422bc527f07aa5d474dc34cb6983b40962b
lyylyylyylyy <lyyzju@outlook.com>
2020-05-15 23:04:56 +0800

    Updates

---
+++ precache-manifest.750cd82d17b9144ca365a79fe6cbcb74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaadbf70283bbbda79404e43ec93e47c",
+    "url": "/covid19-map/index.html"
+  },
+  {
+    "revision": "df286c029f6529687ed0",
+    "url": "/covid19-map/static/css/2.d7262850.chunk.css"
+  },
+  {
+    "revision": "bae4b92a834f0c0322a1",
+    "url": "/covid19-map/static/css/main.3e695bc0.chunk.css"
+  },
+  {
+    "revision": "df286c029f6529687ed0",
+    "url": "/covid19-map/static/js/2.18e1f195.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19-map/static/js/2.18e1f195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae4b92a834f0c0322a1",
+    "url": "/covid19-map/static/js/main.9262c2c5.chunk.js"
+  },
+  {
+    "revision": "cbce285d6aadc9b98389",
+    "url": "/covid19-map/static/js/runtime-main.5037be77.js"
+  }
+]);
