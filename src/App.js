89a414c3f86f23fd8772e1da142cb870b1a17e4b
lyylyylyylyy <lyyzju@outlook.com>
2020-05-15 23:03:37 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React ,  { Component } from 'react';
-import './App.css';
 import Map from './components/Map';
 import WorldData from './components/WorldData';
+import { Tabs } from 'antd';
+import 'antd/dist/antd.css';
+
+const { TabPane } = Tabs;
 
 class App extends Component {
+
   componentDidMount() {
     
   }
 
   render() {
+
     return (
-      <div>
-        <div className = "data1">
+      <Tabs defaultActiveKey="1" tabPosition="bottom">
+        <TabPane tab="世界疫情概览" key="1">
           <WorldData></WorldData>
-        </div>
-        <div className="App">
-          <span id = "title">新冠肺炎世界疫情地图</span>
-          <Map className = "map"></Map>
-        </div>
-      </div>
+        </TabPane>
+        <TabPane tab="世界疫情地图" key="2">
+          <Map></Map>
+        </TabPane>
+      </Tabs>
     );
   }
 }
